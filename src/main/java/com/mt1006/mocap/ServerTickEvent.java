0b5401c7b7a4bd00557a20ba8ad8de4125eda8c1
mt1006 <damianm916@gmail.com>
2022-08-05 20:55:12 +0200

    Commit

---
+++ src/main/java/com/mt1006/mocap/ServerTickEvent.java
@@ -0,0 +1,41 @@
+package com.mt1006.mocap;
+
+import com.mt1006.mocap.mocap.PlayerState;
+import com.mt1006.mocap.mocap.Playing;
+import com.mt1006.mocap.mocap.Recording;
+import net.minecraft.Util;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.server.MinecraftServer;
+
+public class ServerTickEvent
+{
+	private static PlayerState oldPlayerState = null;
+
+	public static void onEndTick(MinecraftServer server)
+	{
+		if (Recording.state == Recording.State.WAITING_FOR_ACTION)
+		{
+			PlayerState playerState = new PlayerState(Recording.serverPlayer);
+			if (!playerState.compare(oldPlayerState))
+			{
+				Recording.previousPlayerState = null;
+				Recording.state = Recording.State.RECORDING;
+				Recording.serverPlayer.sendMessage(new TranslatableComponent("mocap.commands.recording.start.recording_started"), Util.NIL_UUID);
+				oldPlayerState = null;
+			}
+			else
+			{
+				oldPlayerState = playerState;
+			}
+		}
+
+		if (Recording.state == Recording.State.RECORDING)
+		{
+			PlayerState playerState = new PlayerState(Recording.serverPlayer);
+			playerState.saveDifference(Recording.recording, Recording.previousPlayerState);
+			Recording.previousPlayerState = playerState;
+		}
+
+		Playing.onTick();
+	}
+}
