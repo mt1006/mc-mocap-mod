536a0e0bb12344678fed7844fb661ff13412c884
mt1006 <damianm916@gmail.com>
2023-03-15 21:12:04 +0100

    Version for 1.19.4

--- src/main/java/com/mt1006/mocap/MocapMod.java
+++ src/main/java/com/mt1006/mocap/MocapMod.java
@@ -20,7 +20,7 @@ public class MocapMod implements ModInitializer
 {
 	public static final String MOD_ID = "mocap";
 	public static final String VERSION = "1.2.1";
-	public static final String FOR_VERSION = "1.19.3";
+	public static final String FOR_VERSION = "1.19.4";
 	public static final String FOR_LOADER = "Fabric";
 	public static final Logger LOGGER = LogManager.getLogger();
 
