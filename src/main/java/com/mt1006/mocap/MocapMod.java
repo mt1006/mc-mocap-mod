497d539f96ab4b5e0d37a01ec0c7dec6612a0de0
mt1006 <damianm916@gmail.com>
2023-06-13 22:22:14 +0200

    Version for 1.20.1

--- src/main/java/com/mt1006/mocap/MocapMod.java
+++ src/main/java/com/mt1006/mocap/MocapMod.java
@@ -15,7 +15,7 @@ public class MocapMod
 {
 	public static final String MOD_ID = "mocap";
 	public static final String VERSION = "1.2.1";
-	public static final String FOR_VERSION = "1.20";
+	public static final String FOR_VERSION = "1.20.1";
 	public static final String FOR_LOADER = "Forge";
 	public static final Logger LOGGER = LogManager.getLogger();
 
