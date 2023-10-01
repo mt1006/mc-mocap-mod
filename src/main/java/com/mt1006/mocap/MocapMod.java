6cf4f6a903233efa630c20f48bad073760fd15b2
mt1006 <damianm916@gmail.com>
2023-10-01 13:31:40 +0200

    Version 1.3.3

--- src/main/java/com/mt1006/mocap/MocapMod.java
+++ src/main/java/com/mt1006/mocap/MocapMod.java
@@ -22,7 +22,7 @@ import org.apache.logging.log4j.Logger;
 public class MocapMod implements ModInitializer
 {
 	public static final String MOD_ID = "mocap";
-	public static final String VERSION = "1.3.2";
+	public static final String VERSION = "1.3.3";
 	public static final String FOR_VERSION = "1.18.2";
 	public static final String FOR_LOADER = "Fabric";
 	public static final Logger LOGGER = LogManager.getLogger();
