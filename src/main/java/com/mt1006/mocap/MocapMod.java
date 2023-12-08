45219aae8a6a6eee22d815b3d40c890eefa22319
mt1006 <damianm916@gmail.com>
2023-12-08 22:54:43 +0100

    Version 1.3.4

--- src/main/java/com/mt1006/mocap/MocapMod.java
+++ src/main/java/com/mt1006/mocap/MocapMod.java
@@ -20,8 +20,8 @@ import org.apache.logging.log4j.Logger;
 public class MocapMod implements ModInitializer
 {
 	public static final String MOD_ID = "mocap";
-	public static final String VERSION = "1.3.3";
-	public static final String FOR_VERSION = "1.20.1";
+	public static final String VERSION = "1.3.4";
+	public static final String FOR_VERSION = "1.20.2";
 	public static final String FOR_LOADER = "Fabric";
 	public static final Logger LOGGER = LogManager.getLogger();
 	public static final boolean isDedicatedServer = FabricLoader.getInstance().getEnvironmentType() == EnvType.SERVER;
