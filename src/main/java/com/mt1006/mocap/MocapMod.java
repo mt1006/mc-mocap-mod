77bfbacd98a9034f8a5f088b101f15725a179031
mt1006 <damianm916@gmail.com>
2023-12-09 23:14:02 +0100

    Version for 1.20.4

--- src/main/java/com/mt1006/mocap/MocapMod.java
+++ src/main/java/com/mt1006/mocap/MocapMod.java
@@ -17,7 +17,7 @@ public class MocapMod
 {
 	public static final String MOD_ID = "mocap";
 	public static final String VERSION = "1.3.4";
-	public static final String FOR_VERSION = "1.20.2";
+	public static final String FOR_VERSION = "1.20.4";
 	public static final String FOR_LOADER = "Forge";
 	public static final Logger LOGGER = LogManager.getLogger();
 	public static final boolean isDedicatedServer = FMLEnvironment.dist.isDedicatedServer();
