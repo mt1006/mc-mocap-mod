aab547105dd5ade3c2a03690b6e4de13324896bb
mt1006 <damianm916@gmail.com>
2023-12-09 23:15:44 +0100

    Version for 1.20.4

--- src/main/java/com/mt1006/mocap/MocapMod.java
+++ src/main/java/com/mt1006/mocap/MocapMod.java
@@ -21,7 +21,7 @@ public class MocapMod implements ModInitializer
 {
 	public static final String MOD_ID = "mocap";
 	public static final String VERSION = "1.3.4";
-	public static final String FOR_VERSION = "1.20.2";
+	public static final String FOR_VERSION = "1.20.4";
 	public static final String FOR_LOADER = "Fabric";
 	public static final Logger LOGGER = LogManager.getLogger();
 	public static final boolean isDedicatedServer = FabricLoader.getInstance().getEnvironmentType() == EnvType.SERVER;
