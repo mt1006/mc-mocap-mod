81b772333e1dca2c1f0d667fae66b544d0929e8f
mt1006 <damianm916@gmail.com>
2024-06-17 13:25:53 +0200

    Version 1.3.6

--- src/main/java/com/mt1006/mocap/MocapMod.java
+++ src/main/java/com/mt1006/mocap/MocapMod.java
@@ -16,7 +16,7 @@ import org.apache.logging.log4j.Logger;
 public class MocapMod
 {
 	public static final String MOD_ID = "mocap";
-	public static final String VERSION = "1.3.5";
+	public static final String VERSION = "1.3.6";
 	public static final String FOR_VERSION = "1.20.6";
 	public static final String FOR_LOADER = "Forge";
 	public static final Logger LOGGER = LogManager.getLogger();
@@ -30,7 +30,7 @@ public class MocapMod
 	@SubscribeEvent
 	public static void setup(final FMLCommonSetupEvent event)
 	{
-		MocapMod.LOGGER.info(getFullName() + " - Author: mt1006 (mt1006x)");
+		MocapMod.LOGGER.info("{} - Author: mt1006", getFullName());
 		Fields.init();
 		MocapPackets.register();
 		Action.init();
