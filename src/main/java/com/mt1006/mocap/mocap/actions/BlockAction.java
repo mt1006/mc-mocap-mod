15dec173c603c7153ddf68aefbf5b091c66726cf
mt1006 <damianm916@gmail.com>
2024-09-03 13:24:08 +0200

    Version 1.3.9

--- src/main/java/com/mt1006/mocap/mocap/actions/BlockAction.java
+++ src/main/java/com/mt1006/mocap/mocap/actions/BlockAction.java
@@ -48,7 +48,8 @@ public interface BlockAction extends Action
 				level.setBlock(blockPos, blockState, 3);
 
 				SoundType soundType = blockState.getSoundType();
-				level.playSound((Player)entity, blockPos, blockState.getSoundType().getPlaceSound(),
+				Player player = entity instanceof Player ? (Player)entity : null;
+				level.playSound(player, blockPos, blockState.getSoundType().getPlaceSound(),
 						SoundSource.BLOCKS, (soundType.getVolume() + 1.0F) / 2.0F, soundType.getPitch() * 0.8F);
 			}
 		}
