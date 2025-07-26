201f1f69c8fd3f74dbe7bcfe168ba11dec41d362
LOUDO <lucaslelouer@gmail.com>
2025-07-26 22:51:06 +0200

    Update: Get animation data from server-side list, not client-side list.

--- common/src/main/java/net/mt1006/mocap/mocap/actions/Emote.java
+++ common/src/main/java/net/mt1006/mocap/mocap/actions/Emote.java
@@ -1,8 +1,8 @@
 package net.mt1006.mocap.mocap.actions;
 
 import com.zigythebird.playeranimcore.animation.Animation;
-import io.github.kosmx.emotes.api.events.client.ClientEmoteAPI;
 import io.github.kosmx.emotes.api.events.server.ServerEmoteAPI;
+import io.github.kosmx.emotes.server.serializer.UniversalEmoteSerializer;
 import net.mt1006.mocap.api.v1.extension.MocapRecordingData;
 import net.mt1006.mocap.api.v1.extension.actions.MocapAction;
 import net.mt1006.mocap.api.v1.extension.actions.MocapActionContext;
@@ -14,9 +14,7 @@ public class Emote implements MocapAction {
 
     private Animation animation;
 
-    public Emote() {
-        this.animation = null;
-    }
+    public Emote() {}
 
     public Emote(Animation animation) {
         this.animation = animation;
@@ -24,21 +22,17 @@ public class Emote implements MocapAction {
 
     public Emote(Reader reader) {
         try {
-            UUID emoteId = UUID.fromString(reader.readString());
-            for(Animation animation : ClientEmoteAPI.clientEmoteList()) {
-                if(animation.get().equals(emoteId)) {
-                    this.animation = animation;
-                }
-             }
-        } catch (Exception e) {
-            animation = null;
-        }
+            UUID uuid = UUID.fromString(reader.readString());
+            animation = UniversalEmoteSerializer.getEmote(uuid);
+        } catch (Exception ignored) {}
     }
 
     @Override
     public void write(Writer writer, MocapRecordingData data) {
-        if(animation != null) {
-            writer.addString(animation.get().toString());
+        if(animation == null) {
+            writer.addString("");
+        } else {
+            writer.addString(animation.data().get("uuid").get().toString());
         }
     }
 
