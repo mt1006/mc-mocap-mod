8f10070c7460ab5ffb1404b227cd51ef82d4d9ea
mt1006 <mt1006@proton.me>
2025-10-31 01:10:38 +0100

    Restore LivingEntityRendererMixin - fix hiding empty name bars not working

---
+++ common/src/main/java/net/mt1006/mocap/mixin/LivingEntityRendererMixin.java
@@ -0,0 +1,23 @@
+package net.mt1006.mocap.mixin;
+
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntityRenderer.class)
+public class LivingEntityRendererMixin
+{
+	@Inject(method = "shouldShowName(Lnet/minecraft/world/entity/LivingEntity;)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;getTeam()Lnet/minecraft/world/scores/PlayerTeam;"), cancellable = true)
+	private void atShouldShowName(LivingEntity entity, double distanceSq, CallbackInfoReturnable<Boolean> cir)
+	{
+		if (entity instanceof AbstractClientPlayer && ((AbstractClientPlayer)entity).getGameProfile().name().isEmpty())
+		{
+			cir.setReturnValue(false);
+			cir.cancel();
+		}
+	}
+}
